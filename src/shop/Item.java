ff372bd5c64a3594dca1893b6ef894105a3904d6
BoscoMurray <rossamurray@gmail.com>
2017-07-23 12:27:05 +0100

    initial commit

---
+++ src/shop/Item.java
@@ -0,0 +1,15 @@
+package shop;
+
+/**
+ * Created by user on 23/07/2017.
+ */
+public class Item {
+    private String name;
+    private Double price;
+
+    public Item(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+}
---
+++ tests/ItemTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by user on 23/07/2017.
+ */
+public class ItemTest {
+}
