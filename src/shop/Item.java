4aea6b1c68aafe99cd896be3af29426f127751aa
BoscoMurray <rossamurray@gmail.com>
2017-07-23 12:42:08 +0100

    item tests OK

--- src/shop/Item.java
+++ src/shop/Item.java
@@ -12,4 +12,11 @@ public class Item {
         this.price = price;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
 }
--- tests/ItemTest.java
+++ tests/ItemTest.java
@@ -1,5 +1,29 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import shop.Item;
+
 /**
  * Created by user on 23/07/2017.
  */
 public class ItemTest {
+
+    Item apple;
+
+    @Before
+    public void before() {
+        apple = new Item("Apple", 0.50);
+    }
+
+    @Test
+    public void testGetName() {
+        String name = apple.getName();
+        Assert.assertEquals("Apple", name);
+    }
+
+    @Test
+    public void testGetPrice() {
+        Double price = apple.getPrice();
+        Assert.assertEquals(0.50, price, 0.001);
+    }
 }
